bcd73045771514ebfe260606f3db09b1756318aa
ilias <netrade088@gmail.com>
2022-11-08 16:51:48 +0200

    cbnjcm

---
+++ 123123123.py
@@ -0,0 +1,66 @@
+class Animal:
+    def __init__(self, name, ves, paroda, sitost):
+        self.name = name
+        self.ves = ves
+        self.paroda = paroda
+        self.sitost = sitost
+
+
+    def sitost(self, sitost):
+        self.sitost
+
+
+
+    def golos(self):
+        pass
+
+    def ShowInfo(self):
+        print("Name: ", self.name, "Paroda: ", self.paroda, "Ves: ", self.ves,)
+
+
+
+class Dog(Animal):
+    def __init__(self, name, ves, paroda,):
+        super().__init__(name, ves, paroda,)
+
+
+    def golos(self):
+        print("Gav Gav")
+
+class Cat(Animal):
+    def __init__(self, name, ves, paroda):
+        super().__init__(name, ves, paroda,)
+
+    def golos(self):
+        print("Mau Mau")
+
+
+mydog = Dog("bobik", 20, "Labrador")
+mydog.ShowInfo()
+mydog.golos()
+myCat = Cat("Barsik", 5, "Bengal")
+myCat.ShowInfo()
+myCat.golos()
+
+class CatDog(Cat, Dog):
+    pass
+
+catDog = CatDog("bill", 40, "catdog")
+catDog.ShowInfo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
